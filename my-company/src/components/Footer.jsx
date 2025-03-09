3e7eb24f6cce4b2a1c0fd10e11d332e0b4529244
Ochalifu Binta Fatima <ochalifubinta01@gmail.com>
2025-03-09 21:28:06 +0100

    added mycompany

--- my-company/src/components/Footer.jsx
+++ my-company/src/components/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
-    return (
-      <footer style={{ padding: '10px', background: '#eee', textAlign: 'center' }}>
-        <p>© 2025 My Company. All rights reserved.</p>
-      </footer>
-    );
-  }
-  
-  export default Footer;
+  return (
+    <footer style={{ padding: '10px', background: '#eee', textAlign: 'center' }}>
+      <p>© 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
