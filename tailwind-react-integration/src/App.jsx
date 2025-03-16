66e8d05ea1584a64240ea1575dda48bf220d0f30
Ochalifu Binta Fatima <ochalifubinta01@gmail.com>
2025-03-16 21:29:59 +0100

    added tailwindfolder

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserProfile from "./UserProfile";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-200">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
